cb3558515278d6c39ee1135f9eeae6a3158e1be2
borja <brandonon266@gmail.com>
2024-03-25 13:27:53 -0400

    borja

--- Consulta #2.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM producto WHERE id_fabricante=(SELECT id FROM fabricante WHERE nombre='Lenovo');
-SELECT * FROM producto WHERE precio=(SELECT max(precio) FROM producto WHERE id_fabricante=(SELECT id FROM fabricante WHERE nombre='Lenovo'));
-SELECT nombre FROM producto WHERE precio=(SELECT MAX(precio)FROM producto WHERE id_fabricante=(SELECT id FROM fabricante WHERE nombre='Lenovo'));
-SELECT nombre FROM producto WHERE precio=(SELECT MIN(precio)FROM producto WHERE id_fabricante=(SELECT id FROM fabricante WHERE nombre='Hewlett-Packard'));
-SELECT * FROM producto WHERE precio=(SELECT precio FROM producto WHERE  )
